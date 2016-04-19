f1b1dd646a7254a511f37b97bc9c871ad1dc1c12
Hsieh Meng Hua <qprs71@gmail.com>
2016-04-19 22:56:38 +0800

    隨機的阿敏跑出來

---
+++ js/profile.js
@@ -0,0 +1,25 @@
+$(function(){
+    
+    var picList = ['images/Amin/Amin1.jpg',
+                   'images/Amin/Amin2.jpg',
+                   'images/Amin/Amin3.jpg',
+                   'images/Amin/Amin4.jpg',
+                   'images/Amin/Amin5.jpg',
+                   'images/Amin/Amin6.jpg'];
+    
+    var i;
+    
+    // Show the initial images
+    refreshImage('.left-pic');
+    
+    $('.right-pic').css("background-image", "url(images/MrFrog.jpg)");
+
+    function refreshImage(which){
+        i = getRandomInt(0,picList.length-1);
+        $(which).css("background-image", "url(" + picList[i] + ")");
+    }
+  
+    function getRandomInt(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+});
