5cf13ff39f757d2f5be1b320a3da82e1dca56fc2
Hsieh Meng Hua <qprs71@gmail.com>
2016-04-19 13:25:02 +0800

    改圖片路徑

--- js/switch_picture.js
+++ js/switch_picture.js
@@ -1,19 +1,19 @@
 $(function(){
     
-    var picList = ['../images/food01.jpg',
-                   '../images/food02.jpg',
-                   '../images/food03.jpg',
-                   '../images/food04.jpg',
-                   '../images/food05.jpg',
-                   '../images/food06.jpg',
-                   '../images/food07.jpg',
-                   '../images/food08.jpg',
-                   '../images/food09.jpg',
-                   '../images/food10.jpg',
-                   '../images/food11.jpg',
-                   '../images/food12.jpg',
-                   '../images/food13.jpg',
-                   '../images/food14.jpg'];
+    var picList = ['images/food01.jpg',
+                   'images/food02.jpg',
+                   'images/food03.jpg',
+                   'images/food04.jpg',
+                   'images/food05.jpg',
+                   'images/food06.jpg',
+                   'images/food07.jpg',
+                   'images/food08.jpg',
+                   'images/food09.jpg',
+                   'images/food10.jpg',
+                   'images/food11.jpg',
+                   'images/food12.jpg',
+                   'images/food13.jpg',
+                   'images/food14.jpg'];
     
     var i;
     
